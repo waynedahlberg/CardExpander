12d5a137bbd80e2c70a0d96bd990a57ded491438
Wayne Dahlberg <waynefj40@gmail.com>
2024-09-23 17:19:24 -0600

    Initial Commit

---
+++ CardExpander/CardExpanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardExpanderApp.swift
+//  CardExpander
+//
+//  Created by Wayne Dahlberg on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CardExpanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
